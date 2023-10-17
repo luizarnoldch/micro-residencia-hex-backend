58ffa810ead805c69bdbe8ffaa46a23db949f8b1
luizarnoldch <luizarnoldch@gmail.com>
2023-10-17 16:51:47 -0500

    lambda por APIGATEWAY

--- lambdas/hello/main.go
+++ lambdas/hello/main.go
@@ -4,14 +4,20 @@ import (
 	"context"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context) (string, error) {
-	
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
 	log.Println("Hello world with cognito")
 
-	return "Hello World, Protected", nil
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       "Hello World, Protected",
+	}
+	return response, nil
 }
 
 func main() {
