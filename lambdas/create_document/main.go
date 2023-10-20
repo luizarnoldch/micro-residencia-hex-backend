4d1074ca7ddad617440dc1da8f20e20c6e64a220
luizarnoldch <luizarnoldch@gmail.com>
2023-10-20 01:10:31 -0500

    test create document v0.25

--- lambdas/create_document/main.go
+++ lambdas/create_document/main.go
@@ -8,9 +8,9 @@ import (
 
 	"log"
 
-	// "main/src/application"
+	"main/src/application"
 	"main/src/domain"
-	// "main/src/infrastructure"
+	"main/src/infrastructure"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -21,33 +21,12 @@ var (
 )
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// dynamoClient, err := infrastructure.GetDynamoClient(ctx)
-	// if err != nil {
-	// 	log.Fatalln("Failed to get dynamodb client")
-	// 	return events.APIGatewayProxyResponse{Body: fmt.Sprintf("Failed to get dynamodb client %s", err),
-	// 		StatusCode: 504}, nil
-	// }
-
-	// // var documentoRequest domain.DocumentoRequest
-
-	// // if err := json.Unmarshal([]byte(request.Body), &documentoRequest); err != nil {
-	// // 	log.Println("Error parsing request body as JSON.")
-	// // 	return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 502}, nil
-	// // }
-
-	// // dynamoService := application.NewDocumentoServiceDynamo(dynamoClient, TABLE_NAME, ctx)
-
-	// // response, err := dynamoService.CreateDocument(documentoRequest)
-	// // if err != nil {
-	// // 	log.Printf("error creating documento in database :%s\n", err)
-	// // 	return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 400}, nil
-	// // }
-
-	// // responseBody, err := json.Marshal(response)
-	// // if err != nil {
-	// // 	log.Printf("error marshaling response to JSON: %s\n", err)
-	// // 	return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 500}, nil
-	// // }
+	dynamoClient, err := infrastructure.GetDynamoClient(ctx)
+	if err != nil {
+		log.Fatalln("Failed to get dynamodb client")
+		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("Failed to get dynamodb client %s", err),
+			StatusCode: 504}, nil
+	}
 
 	var documentoRequest domain.DocumentoRequest
 
@@ -56,13 +35,20 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 502}, nil
 	}
 
-	reqToDoc := documentoRequest.ToDocumento()
+	dynamoService := application.NewDocumentoServiceDynamo(dynamoClient, TABLE_NAME, ctx)
 
-	responseBody, err := json.Marshal(reqToDoc)
+	response, err := dynamoService.CreateDocument(documentoRequest)
+	if err != nil {
+		log.Printf("error creating documento in database :%s\n", err)
+		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 400}, nil
+	}
+
+	responseBody, err := json.Marshal(response)
 	if err != nil {
 		log.Printf("error marshaling response to JSON: %s\n", err)
 		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%s", err), StatusCode: 500}, nil
 	}
+
 
 	return events.APIGatewayProxyResponse{
 		Headers:    map[string]string{"Content-Type": "application/json"},
