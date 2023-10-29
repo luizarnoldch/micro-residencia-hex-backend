5b44ecfcff9a3bc660c4d5927d2e498b3b380a50
luizarnoldch <luizarnoldch@gmail.com>
2023-10-29 17:16:23 -0500

    test create document v0.95

--- lambdas/filter_document/main.go
+++ lambdas/filter_document/main.go
@@ -105,7 +105,15 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	}
 
 	log.Println("Handler completed successfully")
+	headers := map[string]string{
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "DELETE,GET,HEAD,POST,PUT",
+		"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+		"Content-Type": "application/json",
+	}
+
 	return events.APIGatewayProxyResponse{
+		Headers:    headers,
 		Body:       string(body),
 		StatusCode: 200,
 	}, nil
